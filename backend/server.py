32ed00b2ebc3c639b796970552ecd6cbf49a294d
podoslamet20-debug <podoslamet20@gmail.com>
2026-07-31 11:39:55 +0700

    Update server.py
    Fix: Add # prefix to comment

--- backend/server.py
+++ backend/server.py
@@ -2924,6 +2924,6 @@ app.add_middleware(
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
-    Register API router with /api prefix
-app.include_router(api_router)
+# Register API router with /api prefix
+    app.include_router(api_router)
     client.close()
